932be059f837b37a3f68d8b4e45bcef6391d6128
Myles Spencer <mspencer.mes@gmail.com>
2019-10-05 12:47:32 -0400

    Added hidden to display helper

--- src/display.js
+++ src/display.js
@@ -3,9 +3,13 @@ export default {
     display: 'block',
     width: '100%',
     boxSizing: 'border-box',
+    resize: 'vertical',
   },
   inline: {
     display: 'inline-block',
     boxSizing: 'border-box',
+  },
+  hidden: {
+    display: 'none', // TODO: Change this to something more a11y friendly
   },
 };
