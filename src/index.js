7e28dde3e676ff5351ea505d618d2f653de94387
Myles Spencer <mspencer.mes@gmail.com>
2020-01-25 12:37:27 -0500

    Updated exports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
-import aphroditeInterface from 'react-with-styles-interface-aphrodite/no-important';
-import theme from './theme';
-import TextInput from './text-input';
-import DateInput from './date-input';
-
-ThemedStyleSheet.registerInterface(aphroditeInterface);
-ThemedStyleSheet.registerTheme(theme);
-
+export theme from './theme';
 export Box from './box';
 export Button from './button';
 export Link from './link';
